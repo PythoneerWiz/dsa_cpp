63dfc2662706e177178831a435ddda247b6ae370
Mayank Singh <singhmayank55667@gmail.com>
2024-03-14 22:08:15 +0530

    array

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main(){
+//      int a,b;
+//   cin>>a>>b;
+//   if(a>b){
+//     cout<<"A is positive";
+
+// }
+// else if(a<b){
+//     cout<<"A is negative";
+// }
+// else{
+//     cout<<"A is invalid";
+// }
+// char ch;
+// cin>>ch;
+// if(ch>='A'&&ch<='Z'){
+//     cout<<"ch is uppercase";
+// }
+// else if(ch>='a'&&ch<='z'){
+//     cout<<"ch is lowecase";
+
+// }
+// else if(ch>='0'&&ch<='9'){
+//     cout<<"ch is digit";
+// }
+// else {
+//     cout<<"invalid";
+// }
+// int n;
+// cin>>n;
+// int i=2;
+// int sum=0;
+// while(i<=n){
+//    sum=sum+i;
+//     i=i+2;
+// }
+// cout<<sum; 
+
+int n;
+cin>>n;
+int i=1;
+while(i<=n){
+    int j=1;
+    while(j<=n){
+        cout<<i;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+
+}
+ 
+}
+ 
+ 
+  
+
