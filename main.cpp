45c047a2c898ecaa414249c6001823f0b1d976ae
Mayank Singh <singhmayank55667@gmail.com>
2024-03-17 12:47:18 +0530

    mayank

--- main.cpp
+++ main.cpp
@@ -149,11 +149,52 @@ int main(){
 //     row=row+1;
 // }
 
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n){
+//   int col=1;
+//   while(col<=row){
+//     char ch='A'+row-1;
+//     cout<<ch;
+//     col=col+1;
 
 
+//   }
+//   cout<<endl;
+//   row=row+1;
+
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n){
+//   int col=1;
+//   char value='A';
+//   while(col<=row){
+//     cout<<value;
+//     value=value+1;
+//   }
+//   cout<<endl;
+//   row=row+1;
+// }
+// int n;
+// cin>>n;
+// int row=1;
+// while(row<=n){
+//   int col=1;
+//   while(col<=row){
+//     char ch='A'+row+col-2;
+//     cout<<ch;
+//     col=col+1;
+//   }
+//   cout<<endl;
+//   row=row+1;
 
  
-  }
+}
+
+
+  
 
  
  
