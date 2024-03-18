2e0ee8390782ad36de53d2057b2072766f18faf1
Mayank Singh <singhmayank55667@gmail.com>
2024-03-18 23:59:12 +0530

    mayank

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 int main(){
 //      int a,b;
@@ -202,7 +203,18 @@ int main(){
   //   cout<<"prime number";   
   // }
 
+// int n;
+// cin>>n;
+// int ans= 0;
+// int i= 0;
+// while(n !=0){
+//   int bit = n & 1;
+//   ans=(bit * pow(10,i)) + ans;
+//   n=n >>  1;
+//   i++;
 
+// }
+// cout<<ans;
 }
 
  
