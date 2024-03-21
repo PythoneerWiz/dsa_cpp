00970d281cd5e09635df20706f9b1fdf32d9c695
Mayank Singh <singhmayank55667@gmail.com>
2024-03-21 19:21:13 +0530

    login signin

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-int main(){
-//      int a,b;
+// int main(){
+// //      int a,b;
 //   cin>>a>>b;
 //   if(a>b){
 //     cout<<"A is positive";
@@ -216,22 +216,174 @@ int main(){
 // }
 // cout<<ans;
 
-char ch='1';
-int num=2;
-cout<<endl;
-switch(num){
-  case 1: cout<<"first";
-            break;
-  case 2: cout<<"second"<<endl;
-            break;
-  default:cout<<"default case";
+// char ch='1';
+// int num=2;
+// cout<<endl;
+// switch(num){
+//   case 1: cout<<"first";
+//             break;
+//   case 2: cout<<"second"<<endl;
+//             break;
+//   default:cout<<"default case";
 
-}
-cout<<endl;
-return 0;
+// }
+// cout<<endl;
+// return 0;
+
+// int a,b;
+// cout<<"enter the value of a"<<endl;
+// cin>>a;
+// cout<<"enter the value of b"<<endl;
+// cin>>b;
+
+// char op;
+// cout<<"enter the operation"<<endl;
+// cin>>op;
+
+// switch(op){
+//   case '+': cout<<(a+b)<<endl;
+//             break;
+
+//   case '-': cout<<(a-b)<<endl;
+//             break;
+
+//   case '*': cout<<(a*b)<<endl;
+//             break;
+
+//   case '/': cout<<(a/b)<<endl;
+//             break;
 
 
-}
+//   default:cout<<"invalid case"<<endl;
+
+
+// }
+// return 0;
+// int amount;
+//     cout<<"Enter the Amount : "<<endl;
+//     cin>>amount;
+
+//     int num = 1;
+//     int note = 0;
+//     switch (num){
+//         case 1: {
+//             note = amount/100;
+//             amount = amount - (100 * note);
+//             cout<<note<<" Rs.100 note requied."<<endl;
+//         }
+//         case 2:{
+//             note = amount/50;
+//             amount = amount - (50 * note);
+//             cout<<note<<" Rs.50 note requied."<<endl;
+//         }
+//         case 3:{
+//             note = amount/20;
+//             amount = amount - (20 * note);
+//             cout<<note<<" Rs.20 note requied."<<endl;
+//         }
+//         case 4:{
+//             note = amount/1;
+//             amount = amount - (1 * note);
+//             cout<<note<<" Rs.1 note requied."<<endl;
+//         }
+//     }
+
+
+// }
+
+// int sum(int a,int b){
+//   int c=pow(a,b);
+//   return c;
+
+// }
+// int main(){
+//   int num1;
+//   cout<<"enter the value of num1"<<endl;
+//   cin>>num1;
+//   int num2;
+//   cout<<"enter the value of num2"<<endl;
+//   cin>>num2;
+//   cout<<pow(num1,num2)<<endl;
+//   return 0;
+
+
+// bool isEven(int a){
+//   if(a&1){
+//     return 0;
+//   }
+//   else{
+//     return 1;
+//   }
+
+
+// }
+ 
+// int main() {
+//   cout<<"enter the value of num"<<endl;
+//   int num;
+//   cin>>num;
+//   if (isEven(num)){
+//     cout<<"even"<<endl;
+    
+//   }
+//   else{
+//     cout<<"odd"<<endl;
+//   }
+   
+
+// }
+
+// int factorial(int n){
+//   int fact=1;
+//   for(int i=1;i<=n;i++){
+//     fact=fact*i;
+
+//       }
+//       return fact;
+
+// }
+
+// int main(){
+//   cout<<"enter the value of num"<<endl;
+//   int num;
+//   cin>>num;
+//   int ans;
+//   cout<<factorial(num)<<endl;
+
+// }
+// int factorial(int n){
+//   int fact=1;
+//   for(int i=1;i<=n;i++){
+//     fact=fact*i;
+
+//       }
+//       return fact;
+
+// }
+
+// int nCr(int n,int r){
+//   int num=factorial(n);
+//   int den=factorial(r) * factorial(n-r);
+//   int ans=num/den;
+//   return ans;
+
+// }
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   int r;
+//   cin>>r;
+//   cout<<nCr(n,r)<<endl;
+
+// }
+
+
+
+
+
+
+
 
  
 
